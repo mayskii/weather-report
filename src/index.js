1ad86e0b6bdfea6af9b6098d3b255a2f01466d33
Maya Zakharova <mayzakharova@gmail.com>
2025-12-03 13:59:30 -0600

    add wave3

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
 'use strict';
+
+const state = {
+  cityNameInput: null,
+  headerCityNameEl: null,
+  resetCityBtn: null,
+
+};
+
+const loadControls = () => {
+  state.cityNameInput = document.getElementById('cityNameInput');
+  state.headerCityNameEl = document.getElementById('headerCityName');
+  state.resetCityBtn = document.getElementById('cityNameReset');
+};
+
+const updateCityName = () => {
+  state.headerCityNameEl.textContent = state.cityNameInput.value;
+};
+
+const resetCityName = () => {
+  state.cityNameInput.value = 'Seattle';
+  updateCityName();
+};
+
+
+const registerEvents = () => {
+  state.cityNameInput.addEventListener('input', updateCityName);
+  state.resetCityBtn.addEventListener('click', resetCityName);
+
+};
+
+const onload = () => {
+  loadControls();
+  registerEvents();
+  updateCityName();
+};
+
+onload();
